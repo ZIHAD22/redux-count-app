dc6f58ee234d8163261a13a165601a8cba64d3d7
ZIHAD22 <mdzihad2244@gmail.com>
2022-09-19 15:12:04 +0600

    add user and get user

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const { createStore } = require("redux");
 // constant
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+const GET_USERS = "GET_USERS";
+const ADD_PRODUCT = "ADD_PRODUCT";
+const GET_PRODUCTS = "GET_PRODUCTS";
 
 // state
 const initialState = {
+  users: [],
   count: 0,
 };
 
+const initialProductsState = {
+  products: [],
+};
+
 // action
-const action = (acctionType) => {
+const action = (actionType, payload) => {
   return {
-    type: acctionType,
+    type: actionType,
+    payload,
   };
 };
 
-// reducer
+// reducer for user and counter
 const countReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
@@ -28,9 +38,21 @@ const countReducer = (state = initialState, action) => {
         ...state,
         count: state.count - 1,
       };
+    case ADD_USER:
+      if (Object.keys(action.payload).length !== 0) {
+        return {
+          ...state,
+          users: [...state.users, action.payload],
+        };
+      }
+
+    case GET_USERS:
+      return {
+        ...state.users,
+      };
 
     default:
-      state;
+      return state;
   }
 };
 
@@ -52,3 +74,9 @@ store.dispatch(action(INCREMENT));
 store.dispatch(action(INCREMENT));
 store.dispatch(action(INCREMENT));
 store.dispatch(action(INCREMENT));
+store.dispatch(action(ADD_USER, { name: "md zihad" }));
+store.dispatch(action(ADD_USER, { name: "md kowser" }));
+store.dispatch(action(ADD_USER, { name: "md kader" }));
+store.dispatch(action(ADD_USER, { name: "md ridoy" }));
+store.dispatch(action(ADD_USER, { name: "md rony" }));
+store.dispatch(action(GET_USERS));
